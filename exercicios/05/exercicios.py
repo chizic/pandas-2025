7262311a7827303a4eedac0ef45f60fd0d03d8c0
Teo Calvo <teocalvo2@gmail.com>
2025-03-14 13:24:41 -0300

    feat: dia 05 encerrado

--- exercicios/05/exercicios.py
+++ exercicios/05/exercicios.py
@@ -31,3 +31,21 @@ clientes.head()
 clientes.sort_values(by="qtdePontos", ascending=False).head(1)["idCliente"]
 
 clientes.sort_values(by="qtdePontos", ascending=True).head(1)["idCliente"]
+
+# %%
+# 05.05 - Selecione a primeira transação diária de cada cliente.
+
+import pandas as pd
+
+transacoes = pd.read_csv("../../data/transacoes.csv")
+transacoes.head()
+
+transacoes["data"] = pd.to_datetime(transacoes["dtCriacao"]).dt.date
+transacoes = transacoes.sort_values("dtCriacao")
+
+# %%
+
+first = transacoes.drop_duplicates(keep="first", subset=["idCliente", "data"])
+last = transacoes.drop_duplicates(keep="last", subset=["idCliente", "data"])
+
+pd.concat([last, first])
